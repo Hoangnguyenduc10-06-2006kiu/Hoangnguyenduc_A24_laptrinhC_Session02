a3eb341f84629a1eedd5dde57fb9001711240486
Hoangnguyenduc10-06-2006kiu <gochoctap102@gmail.com>
2024-11-21 02:07:28 +0700

    upload session02-2

---
+++ ss1-2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int numberFirt,total,numberScond;
+	numberFirt = 5;
+	numberScond = 10; 
+	total=numberFirt+numberScond; 
+
+	printf("tong la:%d",total) ;
+	return 0; 
+}
+
