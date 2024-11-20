e2cd2aa17bd37d9f15fecb89f8160ac3175c7d9c
Hoangnguyenduc10-06-2006kiu <gochoctap102@gmail.com>
2024-11-21 01:31:38 +0700

    upload session02-1

---
+++ ss1-1.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+int main(){
+	int age; // khai bao kieu du lieu so nguyen 
+	float salary; // khai bao kieu du lieu so thuc
+	char grade; // khai bao kieu du lieu ki tu
+	double height; // khai bao kieu du lieu so thuc chinh xac hon 
+	return 0;  
+} 
