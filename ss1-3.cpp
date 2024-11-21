461d3aed591ffd07267bcfc7f275dee43e7e5a73
Hoangnguyenduc10-06-2006kiu <gochoctap102@gmail.com>
2024-11-21 12:35:28 +0700

    upload session02-3

---
+++ ss1-3.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int total,Subtraction,Multiplication,Division,numberFirt,numberScond;
+	numberFirt=10;
+	numberScond=20;
+	printf("so dau tien: %d \n ",numberFirt);
+	printf("so thu hai: %d \n",numberScond); 
+	total=numberFirt+numberScond;
+	Subtraction=numberFirt-numberScond;
+	Multiplication=numberFirt*numberScond;
+	Division=numberFirt/numberScond;
+	printf("tong: %d \n ",total);
+	printf("hieu: %d \n ",Subtraction);
+	printf("tich: %d \n ",Multiplication);
+	printf("thuong: %d \n ",Division);
+	return 0; 
+} 
