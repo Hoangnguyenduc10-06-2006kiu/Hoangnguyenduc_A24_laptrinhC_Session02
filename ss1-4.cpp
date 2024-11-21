e5ab0876a472ffdea5eb5d1f1effd1f70f13c8fc
Hoangnguyenduc10-06-2006kiu <gochoctap102@gmail.com>
2024-11-21 12:35:44 +0700

    upload session02-4

---
+++ ss1-4.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main(){
+	int canh,chuvi,dientich;
+	canh=20;
+	chuvi=canh * 4; 
+	dientich=canh*canh;
+	printf("chu vi hinh vuong la: %d\n",chuvi) ;
+	printf("dien tich hinh vuong la: %d\n",dientich) ;
+	return 0; 
+	
+
+} 
