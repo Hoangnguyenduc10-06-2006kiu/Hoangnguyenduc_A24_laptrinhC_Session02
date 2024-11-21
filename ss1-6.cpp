f280325f66edcb0f7fc7d3837c738d3194584e9c
Hoangnguyenduc10-06-2006kiu <gochoctap102@gmail.com>
2024-11-21 12:36:10 +0700

    upload session02-6

---
+++ ss1-6.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main(){
+	float Pi,bk,chuvi,dientich;
+	Pi=3,14;
+	bk=5,2;
+	chuvi=bk*2*Pi;
+	dientich=bk*bk*Pi;
+	printf("chu vi hinh tron %.2f \n ",chuvi);
+	printf("dien tich hinh tron %.2f \n ",dientich);
+	return 0; 
+}
+	
+
